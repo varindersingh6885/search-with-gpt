55c423d86d6e8f1ea14a68ee8ec347dd8b6b24c1
Varinder Singh <varindersingh6885@gmail.com>
2023-09-23 15:29:37 +0530

    test openai prompting

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,55 @@
-const express = require("express");
+import express from "express";
+import OpenAI from "openai";
+import cors from "cors";
+import dotenv from "dotenv";
+import { projectList } from "./utils/dummyData.js";
 
+dotenv.config();
 const app = express();
 
-const port = 6000;
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
 });
 
-app.listen(port, () => {
-  console.log(`Server started at ${port}`);
+app.get("/projects", async (req, res) => {
+  const { searchQuery } = req.query;
+  if (searchQuery) {
+    const result = await processWithChatGPT(searchQuery);
+    res.send(result);
+  } else {
+    res.send({ data: projectList });
+  }
+});
+
+async function processWithChatGPT(prompt) {
+  const x = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-0613",
+    messages: [
+      { role: "system", content: "Consider the following array of projects" },
+      { role: "system", content: JSON.stringify(projectList.slice(0, 80)) },
+      {
+        role: "user",
+        content: prompt,
+      },
+      {
+        role: "system",
+        content: "Output format: '[<project_title1>, <project_title2>, ...]'",
+      },
+    ],
+  });
+
+  const matchingProjects = x.choices[0].message.content;
+
+  const filteredProjects = projectList.filter((project) => {
+    return matchingProjects.includes(project.title);
+  });
+
+  return { data: filteredProjects };
+}
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
 });
